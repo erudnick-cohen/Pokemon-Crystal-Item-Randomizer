0a83c6d1a556ae469c7a00e583d2a4b9d931fefb
erudnick-cohen <eliot@marlasrudnick.com>
2018-01-21 17:33:07 -0500

    Removed semicolons
    Removed a few semicolons that had been written due to cognitive dissonance

--- TestRandomizationSimple.py
+++ TestRandomizationSimple.py
@@ -5,16 +5,16 @@ import RandomizeItems
 trashItems = LocationData.trashList;
 trashItems.append('Pokedex')
 progressItems = ['Surf', 'Squirtbottle', 'Flash', 'Mystery Egg', 'Rock Smash', 'Cut']
-Zephyr = Badge.Badge();
+Zephyr = Badge.Badge()
 Zephyr.isTrash = False
 Zephyr.Name = 'Zephyr Badge'
-Fog = Badge.Badge();
+Fog = Badge.Badge()
 Fog.isTrash = False
 Fog.Name = 'Fog Badge'
-Hive = Badge.Badge();
+Hive = Badge.Badge()
 Hive.isTrash = False
 Hive.Name = 'Hive Badge'
-Plain = Badge.Badge();
+Plain = Badge.Badge()
 Plain.isTrash = False
 Plain.Name = 'Plain Badge'
 BadgeDict = {'Fog Badge':Fog, 'Zephyr Badge':Zephyr, 'Hive Badge':Hive, 'Plain Badge': Plain}
