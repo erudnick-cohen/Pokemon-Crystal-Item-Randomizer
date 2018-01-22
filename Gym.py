3042dcf58615ba6dd1f91c04ed99ac6f30d764c6
erudnick-cohen <eliot@marlasrudnick.com>
2018-01-22 17:48:05 -0500

    Gym file updates

--- Gym.py
+++ Gym.py
@@ -4,6 +4,7 @@ class Gym(Location.Location):
 		Location.Location.__init__(self,yamlTree)
 		self.badge = None
 		self.NormalBadge = yamlTree["NormalBadge"]
+		self.Code = yamlTree["BadgeLine"]
 	#return if this is a gym or not
 	#This is the gym class
 	def isGym(self):
