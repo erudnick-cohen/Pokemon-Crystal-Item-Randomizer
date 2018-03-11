f4442b05b34b34f199fb65a70dd3efcb9bd14b92
erudnick-cohen <eliot@marlasrudnick.com>
2018-03-11 14:02:59 -0400

    Fixed mssing item locations and added output to track any future ones

--- LoadLocationData.py
+++ LoadLocationData.py
@@ -3,40 +3,39 @@ import Location
 import Gym
 import yaml
 
-LocationList = []
-print("Creating Locations")
-for root, dir, files  in os.walk("Map Data"):
-	for file in files:
-		print("File: "+file)
-		entry = open("Map Data\\"+file,'r')
-		yamlData = yaml.load(entry)
-		print("Locations in file are:")
-		for location in yamlData["Location"]:
-			print(location["Name"])
-			try:
-				LocationList.append(Location.Location(location))
-			except Exception as inst:
-				print("-----------")
-				print("Failure in "+location["Name"])
-				raise(inst)
-				
-print("Creating Gyms")		
-		
-for groot, gdir, gfiles  in os.walk("Gym Data"):
-	for gfile in gfiles:
-		print("File: "+gfile)
-		entry = open("Gym Data\\"+gfile,'r')
-		yamlData = yaml.load(entry)
-		print("Locations in file are:")
-		for location in yamlData["Location"]:
-			print(location["Name"])
-			try:
-				LocationList.append(Gym.Gym(location))
-			except Exception as inst:
-				print("-----------")
-				print("Failure in "+location["Name"])
-				raise(inst)
-
-trashList = []
-for i in LocationList:
-	trashList.extend(i.getTrashItemList())
+def LoadDataFromFolder(path):
+	LocationList = []
+	print("Creating Locations")
+	for root, dir, files  in os.walk(path+"\\Map Data"):
+		for file in files:
+			print("File: "+file)
+			entry = open(path+"\\Map Data\\"+file,'r')
+			yamlData = yaml.load(entry)
+			print("Locations in file are:")
+			for location in yamlData["Location"]:
+				print(location["Name"])
+				try:
+					LocationList.append(Location.Location(location))
+				except Exception as inst:
+					print("-----------")
+					print("Failure in "+location["Name"])
+					raise(inst)
+	print("Creating Gyms")
+	for groot, gdir, gfiles  in os.walk("Gym Data"):
+		for gfile in gfiles:
+			print("File: "+gfile)
+			entry = open(path+"\\Gym Data\\"+gfile,'r')
+			yamlData = yaml.load(entry)
+			print("Locations in file are:")
+			for location in yamlData["Location"]:
+				print(location["Name"])
+				try:
+					LocationList.append(Gym.Gym(location))
+				except Exception as inst:
+					print("-----------")
+					print("Failure in "+location["Name"])
+					raise(inst)
+	trashList = []
+	for i in LocationList:
+		trashList.extend(i.getTrashItemList())
+	return (LocationList,trashList)
--- TestRandomizationSimple.py
+++ TestRandomizationSimple.py
@@ -1,11 +1,13 @@
-import LoadLocationData as LocationData
+import LoadLocationData
 import Badge
 import RandomizeItems
 import RandomizerRom
 import PokemonRandomizer
 from collections import defaultdict
 
-trashItems = LocationData.trashList;
+res = LoadLocationData.LoadDataFromFolder(".")
+trashItems = res[1]
+LocationList = res[0]
 progressItems = ['Surf', 'Squirtbottle', 'Flash', 'Mystery Egg', 'Cut', 'Strength', 'Secret Potion','Red Scale', 'Whirlpool','Card Key', 'Basement Key', 'Waterfall']
 Zephyr = Badge.Badge()
 Zephyr.isTrash = False
@@ -32,7 +34,17 @@ Rising = Badge.Badge()
 Rising.isTrash = False
 Rising.Name = 'Rising Badge'
 BadgeDict = {'Fog Badge':Fog, 'Zephyr Badge':Zephyr, 'Hive Badge':Hive, 'Plain Badge': Plain, 'Storm Badge': Storm, 'Mineral Badge': Mineral, 'Glacier Badge': Glacier, 'Rising Badge': Rising}
-result = RandomizeItems.RandomizeItems('None',LocationData.LocationList,progressItems,trashItems,BadgeDict)
+result = RandomizeItems.RandomizeItems('None',LocationList,progressItems,trashItems,BadgeDict)
+print('-------')
+for j in result[0]:
+	i = result[0][j]
+	if(i.NormalItem is None and i.isItem()):
+		print(i.Name)
+print('-------')
+for j in result[0]:
+	i = result[0][j]
+	if(i.NormalItem is not None and not i.isItem()):
+		print(i.Name)
 monFun = PokemonRandomizer.generateRandomMonFun(result[2],result[0])
 banMap = defaultdict(lambda: [],{'FALKNER 1':['FISHER 11','CLAIR 1'],'BUGSY 1':['CHAMPION 1']})
 newTree = PokemonRandomizer.randomizeTrainers(result[0],85,lambda y: monFun(y,1001,85),True,banMap)
